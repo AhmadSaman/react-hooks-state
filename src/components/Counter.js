0aef59b114a95c60fbc82b911ba0ef8a7d324a81
AhmadSaman <ahmad.00079821@gmail.com>
2021-07-28 14:20:06 +0300

    commit tree 1cdf6beb75022989fb93e899cd51e8ee2ea6239a

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count + 1);
-  }
-
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+	function increment() {
+		setCount((currentCount) => currentCount + 1);
+		setCount((currentCount) => currentCount + 1);
+	}
+	return <div onClick={increment}>{count}</div>;
 }
 
 export default Counter;
